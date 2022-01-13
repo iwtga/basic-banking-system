65b0104aa6f2b7e2b421bc853469ba2062e77ddb
Alan Almeida <alan241015@gmail.com>
2022-01-14 04:00:14 +0530

    Add view customers

--- banksystem/views.py
+++ banksystem/views.py
@@ -1,6 +1,13 @@
-from flask import render_template, url_for
-from banksystem import app
+from flask import render_template, url_for, redirect, flash
+from banksystem import app, db
+from banksystem.models import Customer, Transfers
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/customers')
+def customers():
+    custs = Customer.query.all()
+    print(custs)
+    return render_template('viewcustomers.html', title='Customers', custs=custs)
