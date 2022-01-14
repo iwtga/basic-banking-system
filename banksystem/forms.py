c6168943c1baf57dfeb03c487111c6ac5c0805f2
Alan Almeida <alan241015@gmail.com>
2022-01-15 04:36:49 +0530

    add transfers

--- banksystem/forms.py
+++ banksystem/forms.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms_sqlalchemy.fields import QuerySelectField
 from banksystem.models import Customer
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 def choice_query():
     return Customer.query
 
 class TransferForm(FlaskForm):
     tranferto = QuerySelectField(query_factory=choice_query, allow_blank=False, get_label='name')
+    amount = StringField('Amount', validators=[DataRequired()])
